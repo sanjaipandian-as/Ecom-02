214fa38ab9fbde1d70dae643c8186f8490ec337e
sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-06-10 11:01:32 +0530

    fix: correct Logo import path in LandingPage

--- Frontend/src/components/Customer/LandingPage.jsx
+++ Frontend/src/components/Customer/LandingPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Hero from './Landing/Hero';
 import Categories from './Landing/Categories';
-// import StatsGrid from './Landing/StatsGrid';
-
+import BestSellers from './Landing/BestSellers';
+import Logo from '../Common/Logo';
 
 const LandingPage = () => {
     return (
-        <div className="w-full bg-white pb-20 md:pb-0">
+        <div className="w-full bg-white pb-20 md:pb-0 relative overflow-hidden">
+            {/* Background Decorative Element */}
+            <div className="absolute top-[20%] -left-[10%] w-[40%] aspect-square opacity-[0.02] pointer-events-none transform -rotate-12 select-none">
+                <Logo className="w-full h-full" />
+            </div>
+            
             <Hero />
             <Categories />
-
+            <BestSellers />
         </div>
     );
 };
