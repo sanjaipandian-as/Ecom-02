54e8898e49750b0c228103cbc08f193dd0b0c789
sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-07-10 09:50:41 +0530

    Done

--- Frontend/src/components/Customer/LandingPage.jsx
+++ Frontend/src/components/Customer/LandingPage.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import Hero from './Landing/Hero';
 import Categories from './Landing/Categories';
 import BestSellers from './Landing/BestSellers';
-import Logo from '../Common/Logo';
 
 const LandingPage = () => {
     return (
         <div className="w-full bg-white pb-0 relative overflow-hidden">
-            {/* Background Decorative Element */}
-            <div className="absolute top-[20%] -left-[10%] w-[40%] aspect-square opacity-[0.02] pointer-events-none transform -rotate-12 select-none">
-                <Logo className="w-full h-full" />
-            </div>
-            
             <Hero />
             <Categories />
             <BestSellers />
